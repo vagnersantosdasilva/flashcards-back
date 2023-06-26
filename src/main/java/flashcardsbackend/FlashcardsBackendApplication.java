c4ee04b8ac9b0df90dead705ac4e6590d40a47d7
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2023-06-26 18:57:47 -0300

    first commit

---
+++ src/main/java/flashcardsbackend/FlashcardsBackendApplication.java
@@ -0,0 +1,13 @@
+package flashcardsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flashcardsbackend/FlashcardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package flashcardsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
